47068d7a8145e17495f39f5dfc8240708112a486
Dylan <dylanzhuo6@gmail.com>
2025-04-05 13:27:41 +1100

    Update dark mode slide - Navbar.js and favicon.ico

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -69,13 +69,15 @@ export default function Navbar() {
 
         {/* Theme toggle */}
         <div
-          onClick={toggleTheme}
-          className="w-14 h-7 flex items-center bg-gray-300 dark:bg-gray-700 rounded-full p-1 cursor-pointer transition duration-300"
+            onClick={toggleTheme}
+            className={`w-14 h-7 flex items-center rounded-full p-1 cursor-pointer transition duration-300 ${
+                isDark ? 'bg-green-500' : 'bg-gray-300'
+            }`}
         >
           <div
-            className={`bg-white w-5 h-5 rounded-full shadow-md transform transition-transform duration-300 ${
-              isDark ? 'translate-x-7' : 'translate-x-0'
-            }`}
+              className={`bg-white w-5 h-5 rounded-full shadow-md transform transition-transform duration-300 ${
+                  isDark ? 'translate-x-7' : 'translate-x-0'
+              }`}
           />
         </div>
       </div>
